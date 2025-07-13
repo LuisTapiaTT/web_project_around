076bc024a314079846a4b2c5bd7fd89d4a84249f
LuisTapia <luistapiapintor@gmail.com>
2025-07-12 21:04:37 -0600

    Commit. 21:04 12 Julio.

---
+++ vendor/script.js
@@ -0,0 +1,10 @@
+console.log("Esto es una prueba");
+console.log(`Esto es una
+plantilla literal,
+y no deberia haber un problema con ella; verdad, numero ${8 + 1}?`);
+
+let profileEditButton = document.querySelector(".profile__edit-button");
+
+let profileSaveButton = document.querySelector(".popupWindow__saveButton");
+
+let profileExitEditButton = document.querySelector(".popupWindow__closeButton");
