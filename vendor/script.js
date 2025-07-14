f3ad4793ef346309e8f08f4ae23c57725dc644dd
LuisTapia <luistapiapintor@gmail.com>
2025-07-13 20:26:23 -0600

    Commit. 20:35 13 Julio.

--- vendor/script.js
+++ vendor/script.js
@@ -5,6 +5,27 @@ y no deberia haber un problema con ella; verdad, numero ${8 + 1}?`);
 
 let profileEditButton = document.querySelector(".profile__edit-button");
 
+let inputFields = document.querySelector("popupWindow__form__input");
+
 let profileSaveButton = document.querySelector(".popupWindow__saveButton");
 
-let profileExitEditButton = document.querySelector(".popupWindow__closeButton");
+let profileCloseButton = document.querySelector(".popupWindow__closeButton");
+
+let profileLikeButton  = document.querySelection(".feed__post-likeButton");
+
+console.log(profileCloseButton.hasAttribute("class"));
+
+// Botón de Like.
+if (...) {
+    profileLikeButton.classList.toggle(".feed__post-likeButton-active");
+
+}
+
+// Botón de guardado. 
+if (...) {
+  profileSaveButton.setAttribute("disabled", "token");
+  profileSaveButton.classList.add("popupWindow__saveButton-disabled");
+} else {
+    profileSaveButton.removeAttribute("disabled", "token");
+    profileSaveButton.classList.remove("popupWindow__saveButton-disabled");
+}
