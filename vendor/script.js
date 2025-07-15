5665318550d07992584e152e1a516d7b61a24da7
LuisTapia <luistapiapintor@gmail.com>
2025-07-14 19:21:39 -0600

    Commit. 19:21. 14 Julio.

--- vendor/script.js
+++ vendor/script.js
@@ -3,29 +3,51 @@ console.log(`Esto es una
 plantilla literal,
 y no deberia haber un problema con ella; verdad, numero ${8 + 1}?`);
 
-let profileEditButton = document.querySelector(".profile__edit-button");
+let profileEditButton = document.querySelector(".profile__editButton"); // Botón editar perfil.
 
-let inputFields = document.querySelector("popupWindow__form__input");
+let profileSaveButton = document.querySelector(".popupWindow__saveButton"); // Botón de guardar editar perfil.
 
-let profileSaveButton = document.querySelector(".popupWindow__saveButton");
+let profileCloseButton = document.querySelector(".popupWindow__closeButton"); // Botón de cerrar popup.
 
-let profileCloseButton = document.querySelector(".popupWindow__closeButton");
+let profileLikeButton = document.querySelector(".feed__post-likeButton"); //Botón de like.
 
-let profileLikeButton  = document.querySelection(".feed__post-likeButton");
+let popupWindow = document.querySelector(".popupWindow"); //Ventana popup.
 
-console.log(profileCloseButton.hasAttribute("class"));
+let inputName = document.querySelector(".popupWindow__inputName");
+
+let inputHobby = document.querySelector(".popupWindow__inputHobby");
+
+let infoName = document.querySelector(".profile__name");
+
+let infoHobby = document.querySelector(".profile__hobby");
+
+// Botón editar perfil/abrir popup.
+
+function openPopupWindow() {
+  popupWindow.classList.add("popupWindow-display");
+  inputName.value = infoName.textContent;
+  inputHobby.value = infoHobby.textContent;
+}
+profileEditButton.addEventListener("click", openPopupWindow);
+
+// Botón cerrar popup.
+
+function closePopupWindow() {
+  popupWindow.classList.remove("popupWindow-display");
+}
+profileCloseButton.addEventListener("click", closePopupWindow);
+
+// Botón de guardado.
+function saveProfileInfo(evt) {
+  evt.preventDefault();
+  infoName.textContent = inputName.value;
+  infoHobby.textContent = inputHobby.value;
+}
+
+profileSaveButton.addEventListener("submit", saveProfileInfo);
 
 // Botón de Like.
-if (...) {
-    profileLikeButton.classList.toggle(".feed__post-likeButton-active");
+// if (...) {
+//     profileLikeButton.classList.toggle(".feed__post-likeButton-active");
 
-}
-
-// Botón de guardado. 
-if (...) {
-  profileSaveButton.setAttribute("disabled", "token");
-  profileSaveButton.classList.add("popupWindow__saveButton-disabled");
-} else {
-    profileSaveButton.removeAttribute("disabled", "token");
-    profileSaveButton.classList.remove("popupWindow__saveButton-disabled");
-}
+// }
